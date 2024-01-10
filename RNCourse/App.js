31202044ea84d9900d3df0e0c69801a9db0d1acf
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2024-01-10 21:53:06 +0530

    [21] Flexbox - A Deep Dive

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -1,32 +1,53 @@
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import React from "react";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your course goal" />
-        <Button title="Add Goal" />
+    <View
+      style={{
+        padding: 50,
+        flexDirection: "row",
+        width: "80%",
+        height: 300,
+        justifyContent: "space-around",
+        alignItems: "center",
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "red",
+          //   width: 100,
+          //   height: 100,
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>1</Text>
       </View>
-      <View>
-        <Text>List of Goals...</Text>
+      <View
+        style={{
+          backgroundColor: "blue",
+          //   width: 100,
+          //   height: 100,
+          flex: 2,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>2</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: "green",
+          //   width: 100,
+          //   height: 100,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>3</Text>
       </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  appContainer: {
-    padding: 50,
-  },
-  inputContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: "#cccccc",
-    width: "80%",
-    marginRight: 8,
-    padding: 8,
-  },
-});
